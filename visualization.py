aa91294dba934671b87e3d814ed05ca9832f448a
GorataMbulawa <55438254+GorataMbulawa@users.noreply.github.com>
2019-09-17 12:59:03 +0200

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,1 +1,16 @@
-xxx
+import numpy as np
+data-np.loadtxt(
+    fname='data/inflammation-01.csv',
+    delimiter=','
+)
+import matplotlib as plt
+ave_inflammation =np.mean(
+  data,
+  axix=0
+)
+plt.plot(ave_inflammation)
+max_inflammation=np.max(
+  data,
+  axis=0
+)
+plt.plot(max_inflammation)
